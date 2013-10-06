374912a1744cb75162b3bfbc658eaaa70f977ef3
Pablo Castillo <pabloacastillo@gmail.com>
2013-10-05 23:35:24 -0700

    Solucionado problema del regex que fallaba cuando la fecha de tratamiento tenia dos numeros.
    Esta version del regex de "CUANDO SE TRATARA" acepta fechas en formato 1/ENERO/2013 y 10/ENERO/2013.

--- parser_saca_agenda_senado.py
+++ parser_saca_agenda_senado.py
@@ -17,15 +17,13 @@ print "STATUS: "+str(url.status_code)
 js=json.loads(data)
 for agendas in js:
 	if "AVANCE INFORMATIVO" in agendas["texto"]:
+		print "==================> ID "+agendas["id"]
 		texto= agendas["texto"][agendas["texto"].find("AVANCE INFORMATIVO"):agendas["texto"].find("-----")]
 		print texto
-		match = re.search(r'\d{2}/\d{2}/\d{4}',agendas["texto"])
+		match = re.search(r'\d{1,2}/\d{2}/\d{4}',agendas["texto"])
 		print "===> FECHA PUBLICACION "+match.group(0)
-		
-		# ESTE REGEX ESTA COMENTADO PORQUE SI BIEN EN LOS TESTERS ONLINE FUNCIONA, AL EJECUTARLO NO TRAE RESULTADOS
-		#match=re.search('\d{2}/(?:ENERO|FEBRERO|MARZO|ABRIL|MAYO|JUNIO|JULIO|AGOSTO|SEPTIEMBRE|OCTUBRE|NOVIEMBRE|DICIEMBRE)/\d{4}',texto)
-		#print "===> CUANDO SE TRATARA "+match.group(0)
-		
+		match=re.search('\d{1,2}/(?:ENERO|FEBRERO|MARZO|ABRIL|MAYO|JUNIO|JULIO|AGOSTO|SETIEMBRE|SEPTIEMBRE|OCTUBRE|NOVIEMBRE|DICIEMBRE)/\d{4}',texto)
+		print "===> CUANDO SE TRATARA "+match.group(0)
 		print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 		
 
