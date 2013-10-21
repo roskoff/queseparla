10a4420db6218e442791eae8dea862d35ac367ed
Pablo Castillo <pabloacastillo@gmail.com>
2013-10-21 16:51:19 -0700

    Pequeños cambios para que funcione a partir del JSON de queseparla en lugar de estirar de diputados directamente
    Se hicieron unos cuantos cambios en el origen de los datos, así como modificaciones en los regex y el proceso de lectura/carga de los datos desde el REST de queseparla a guardar en la DB.

--- parse_agenda_diputados.py
+++ parse_agenda_diputados.py
@@ -1,108 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+	#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Este archivo intentan leer el json almacenado en la base de datos, 
+# y re-interpletandolo intenta obtener los datos basicos para generar 
+# una agenda que pueda ser utilizada.
+
 from bs4 import BeautifulSoup
 import requests
-from pyquery import PyQuery as pq
-from lxml import etree
-import bleach
-import re
-import HTMLParser
+import json
 import time
+import datetime
+import re
 import hashlib
 import MySQLdb
 
 
-
 db = MySQLdb.connect(host="localhost", user="root", passwd="toor",db="queseparla", charset = "utf8", use_unicode = True)
+x = db.cursor()
 
-def guardarBD(texto,hora):
-	x = db.cursor()
+url = requests.get("http://tedic.org/queseparla/preagenda/diputados/json/")
+data = url.content
+print "STATUS: "+str(url.status_code)
 
-	hash_texto=hashlib.md5(texto.encode('utf8')).hexdigest()
-	fecha_agregado=time.strftime('%Y-%m-%d %H:%M')
-	x.execute("""INSERT INTO agenda_diputados ( texto, hash_texto, fecha_agregado, hora) VALUES (%s,%s,%s,%s) ON DUPLICATE KEY UPDATE fecha_actualizado=%s""",(texto,hash_texto,fecha_agregado,hora,fecha_agregado))
+js=json.loads(data)
+for agendas in js:
+	html= agendas['html']
+	fecha=re.search('(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d',html)
+	if fecha:
+		fecha_agenda=fecha.group(0)
+		print "FECHA ENCONTRADA: "+str(fecha_agenda)
+		soup = BeautifulSoup(html,"html5lib")
+		losp = soup.find_all('p', attrs = {'style':'text-align: justify;'})
+		for p in losp:
+			lap=p.get_text()
+			horas_presidencia = re.search(r"([0-9]{1,2}\:[0-9]{1,2} Hs\.)",lap)
+			if horas_presidencia:
+				horas_presidencia=horas_presidencia.group(0)
 
+				if horas_presidencia.find("Hs"):
+					horas_presidencia=horas_presidencia[0:horas_presidencia.find("Hs")].strip()
+					fecha_evento=datetime.datetime.strptime(str(fecha_agenda+" "+horas_presidencia),"%d/%m/%Y %H:%M")
+					texto_agenda= (lap).strip(' \n\t').replace('  ','').replace("\n\n","\n").replace("\n\n","\n").replace("\n\n","\n")
+					print "FECHA EVENTO: "+str(fecha_evento)
+					print "TEXTO: "+texto_agenda
+					id_preagenda=agendas['id']
+					texto=texto_agenda
+					hash_texto=hashlib.md5(texto.encode('utf8')).hexdigest()
+					fecha_agregado=time.strftime('%Y-%m-%d %H:%M')
+					fecha_evento=fecha_evento
+					fecha_actualizado=time.strftime('%Y-%m-%d %H:%M')
 
-#DIPUTADOS
-url = requests.get("http://www.diputados.gov.py/ww2/?pagina=agenda-parlamentaria")
-data = url.text
-
-soup = BeautifulSoup(data)
-tabla = soup.find_all('table' , {'class' : 'tex'})
-data = tabla[1].prettify()
-trs = BeautifulSoup(data)
-tds = trs.find_all('td', {'class' : 'borderU'})
-
-rowcount = 0
-for fila in tds:
-	rowcount += 1
-	clean = bleach.clean(fila, tags=[], strip=True)
-	
-	#print clean
-	#cadena = clean
-	cadena = re.sub('\s+',' ',clean.strip())
-	#Fecha
-	if (rowcount == 1):
-		fecha = cadena
-
-	#Presidencia
-	if(rowcount == 2):
-		horas_presidencia = re.findall(r"[0-9][0-9]\:[0-9][0-9] Hs\. ",cadena)
-
-		lista_presidencia = []
-		
-		for x in range(len(horas_presidencia)):
-			
-			inicio = cadena.find(horas_presidencia[x]) + len(horas_presidencia[x])
-			
-			if x < len(horas_presidencia)-1: 
-				fin = cadena.find(horas_presidencia[x+1])
-				lista_presidencia += [cadena[inicio:fin]]
-			else:
-				lista_presidencia += [cadena[inicio:]]
-			
-		presidencia = []
-		print "--------->>"
-		print len(lista_presidencia)
-		print lista_presidencia
-		for p in lista_presidencia:
-			presidencia += [re.sub(r"[0-9][0-9]\:[0-9][0-9] Hs\. ",'',p)]
-		
-	
-	#Comisiones
-	if(rowcount == 3):
-
-		horas_comision = re.findall(r"[0-9][0-9]\:[0-9][0-9] Hs\. ",cadena)
-		
-		lista_comision = []
-		
-		for x in range(len(horas_comision)):
-			
-			inicio = cadena.find(horas_comision[x]) + len(horas_comision[x])
-			
-			if x < len(horas_comision)-1: 
-				fin = cadena.find(horas_comision[x+1])
-				lista_comision += [cadena[inicio:fin]]
-			else:
-				lista_comision += [cadena[inicio:]]
-		comisiones = []
-		for c in lista_comision:
-			comisiones += [re.sub(r"[0-9][0-9]\:[0-9][0-9] Hs\. ",'',c)]
-		
+					x.execute("""INSERT INTO agenda_diputados (id_preagenda,texto,hash_texto,fecha_agregado,fecha_evento) VALUES (%s,%s,%s,%s,%s) ON DUPLICATE KEY UPDATE fecha_actualizado=%s""",(id_preagenda,texto,hash_texto,fecha_agregado,fecha_evento,fecha_actualizado))
 
 
-print "FECHA : " + fecha
+	else:
+		"Y LA FECHA? NO HAY :("
 
-print "PRESIDENCIA : "
-for i in range(len(horas_presidencia)):
-	print "HORA : " + horas_presidencia[i]
-	print "TITULO : " + presidencia[i]
-	guardarBD(presidencia[i],horas_presidencia[i].replace("Hs. ",""))
-
-
-print "COMISIONES : " 
-for i in range(len(horas_comision)):
-	print "HORA : " + horas_comision[i]
-	print "TITULO : " + comisiones[i]
-	guardarBD(comisiones[i],horas_comision[i].replace("Hs. ",""))
+	print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 
 
