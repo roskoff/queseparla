9bd07814cdfe9c5d419cc9aa4f4b945ea0e0e7ca
Pablo Castillo <pabloacastillo@gmail.com>
2014-01-07 10:00:16 -0800

    Pequeños cambios en el regex de fecha
    En la pagina de diputados cambiaron ciertos elementos de html por lo cual se ajusto el regex de fecha.

--- parse_agenda_diputados.py
+++ parse_agenda_diputados.py
@@ -18,7 +18,7 @@ import hashlib
 import MySQLdb
 
 
-db = MySQLdb.connect(host="localhost", user="root", passwd="toor",db="queseparla", charset = "utf8", use_unicode = True)
+db = MySQLdb.connect(host="mysql.tedic.org", user="root", passwd="toor",db="queseparla", charset = "utf8", use_unicode = True)
 x = db.cursor()
 
 url = requests.get("http://tedic.org/queseparla/preagenda/diputados/json/")
@@ -33,31 +33,37 @@ for agendas in js:
 		fecha_agenda=fecha.group(0)
 		print "FECHA ENCONTRADA: "+str(fecha_agenda)
 		soup = BeautifulSoup(html,"html5lib")
-		losp = soup.find_all('p', attrs = {'style':'text-align: justify;'})
+		#losp = soup.find_all('p', attrs = {'style':'text-align: justify;'})
+		losp = soup.find_all('p')
 		for p in losp:
 			lap=p.get_text()
-			horas_presidencia = re.search(r"([0-9]{1,2}\:[0-9]{1,2} Hs\.)",lap)
+			horas_presidencia = re.search(r"([0-9]{1,2}\:[0-9]{1,2}) hs\.",lap,re.IGNORECASE)
+			print lap
 			if horas_presidencia:
 				horas_presidencia=horas_presidencia.group(0)
+				print "HORAS PRESIDENCIA: "+horas_presidencia
 
-				if horas_presidencia.find("Hs"):
-					horas_presidencia=horas_presidencia[0:horas_presidencia.find("Hs")].strip()
+				if horas_presidencia.lower().find("hs"):
+					texto_agenda=(lap).strip(' \n\t').replace('  ','').replace("\n\n","\n").replace("\n\n","\n").replace("\n\n","\n").replace(horas_presidencia,'')
+					horas_presidencia=horas_presidencia[0:horas_presidencia.lower().find("hs")].strip()
 					fecha_evento=datetime.datetime.strptime(str(fecha_agenda+" "+horas_presidencia),"%d/%m/%Y %H:%M")
-					texto_agenda= (lap).strip(' \n\t').replace('  ','').replace("\n\n","\n").replace("\n\n","\n").replace("\n\n","\n")
 					print "FECHA EVENTO: "+str(fecha_evento)
 					print "TEXTO: "+texto_agenda
 					id_preagenda=agendas['id']
-					texto=texto_agenda
+					texto=texto_agenda.strip()
 					hash_texto=hashlib.md5(texto.encode('utf8')).hexdigest()
 					fecha_agregado=time.strftime('%Y-%m-%d %H:%M')
 					fecha_evento=fecha_evento
 					fecha_actualizado=time.strftime('%Y-%m-%d %H:%M')
 
 					x.execute("""INSERT INTO agenda_diputados (id_preagenda,texto,hash_texto,fecha_agregado,fecha_evento) VALUES (%s,%s,%s,%s,%s) ON DUPLICATE KEY UPDATE fecha_actualizado=%s""",(id_preagenda,texto,hash_texto,fecha_agregado,fecha_evento,fecha_actualizado))
-
+				else:
+					print "NO ENCONTRE LA HORA GILUN"
+			else:
+				print "NO HAY HORA NDEE"
 
 	else:
-		"Y LA FECHA? NO HAY :("
+		print "Y LA FECHA? NO HAY :("
 
 	print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
